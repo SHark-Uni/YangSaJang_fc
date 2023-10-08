715fe64fbc952a59dedd64347ecc016d95841878
SHarkkk <wnsrl4067@gmail.com>
2023-10-08 17:07:50 +0900

    #10 - 의존성 업데이트:JPA + MySQL +H2
    - SpringBoot에 MySQL을 사용하게끔 JPA 의존성을 추가함.
    - Mockaroo를 이용한 테스트 데이터를 data.sql에 등록.
    - CRUD TEST를 작성함.

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,18 +1,90 @@
 package com.fastcampus.projectboard.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+    @Setter
+    @Column(nullable = false,length = 10000)
     private String content;
+
+    @Setter
     private String hashtag;
 
+    // 하지만, 댓글 데이터 또한 유용할 수 있다. 추후에 어떠한 방식으로든 이용될 수 있음.
+    // 백업 or 데이터를 이용한 소비자 패턴 분석 등등 ..
+    // 원래 의미론적으로 보면, 게시글을 잃어버린 댓글은 의미가 없음.
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article",cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedBy
+    @Column(nullable = false)
     private String createdBy;
+
+    @LastModifiedBy
+    @Column(nullable = false)
     private String modifiedBy;
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    protected Article(){};
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title,content,hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id !=null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
---
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/repository/JPARepositoryTest.java
@@ -0,0 +1,90 @@
+package com.fastcampus.projectboard.repository;
+
+import com.fastcampus.projectboard.config.JpaConfig;
+import com.fastcampus.projectboard.domain.Article;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import(JpaConfig.class)
+@DisplayName("JPA 연결 테스트")
+@DataJpaTest
+class JPARepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JPARepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Test
+    @DisplayName("Select 테스트")
+    void givenTestData_whenSelecting_thenWorksFine(){
+        //given
+
+        //when
+        List<Article> articles = articleRepository.findAll();
+        //then
+
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+
+    }
+
+    @Test
+    @DisplayName("Insert 테스트")
+    void givenTestData_whenInsert_thenPlusOne(){
+        //given
+        long previousCount = articleRepository.count();
+        Article article = Article.of("new Article","new Content","#Spring");
+        //when
+        Article savedArticle = articleRepository.save(article);
+        //then
+        long afterCount = articleRepository.count();
+        assertThat(afterCount).isEqualTo(previousCount+1);
+
+    }
+
+    @Test
+    @DisplayName("update 테스트")
+    void givenTestData_whenUpdating_thenWorksFine(){
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashTag = "#SpringBootss";
+        article.setHashtag(updatedHashTag);
+        //when
+        Article savedArticle = articleRepository.saveAndFlush(article);
+        //then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag",updatedHashTag);
+
+    }
+
+    @Test
+    @DisplayName("delete 테스트")
+    void givenTestData_whenDeleting_thenWorksFine(){
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long prevArticleCount = articleRepository.count();
+        long prevArticleComment = articleCommentRepository.count();
+
+        int deletedCommentSize = article.getArticleComments().size();
+        //when
+        articleRepository.delete(article);
+        //then
+        assertThat(articleRepository.count()).isEqualTo(prevArticleCount-1);
+        assertThat(articleCommentRepository.count()).isEqualTo(prevArticleComment-deletedCommentSize);
+
+    }
+}
